2c88ccea1a9a47ab8aac3f766e5f2fb61411741e
Dario Randazzo <dario.randazzo@accedo.tv>
2020-02-06 16:00:11 -0500

    Updates

---
+++ precache-manifest.d6bd7e75b975e79204707043816dc131.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7f4b7870113fca429a7b08eb4c19a1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41eb0f5f22ec057ddbd6",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "672dc4d8505e08bf6019",
+    "url": "/static/js/2.1343b054.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.1343b054.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41eb0f5f22ec057ddbd6",
+    "url": "/static/js/main.5a18c526.chunk.js"
+  },
+  {
+    "revision": "818fc7645721419e3281",
+    "url": "/static/js/runtime-main.b59a5bb5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
